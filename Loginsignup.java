9b286223f2657db2574a8c9bd81679f85d8dd9b5
Dejaswini <167988354+Dejaswini@users.noreply.github.com>
2024-11-18 21:28:24 +0530

    Create Loginsignup.java

---
+++ Loginsignup.java
@@ -0,0 +1,14 @@
+
+package loginsignup;
+
+public class Loginsignup {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        login lf=new login();
+        lf.setVisible(true);
+        lf.pack();
+        lf.setLocationRelativeTo(null);
+    }
+    
+}
